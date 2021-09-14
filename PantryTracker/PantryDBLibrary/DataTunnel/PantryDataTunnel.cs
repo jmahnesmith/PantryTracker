cab5ee6a821ef882faf7d95dfae95f4bb64753b2
Josh W Mahnesmith <jwm2166@gmail.com>
2021-09-13 19:01:09 -0500

    Added add food method to PantryDataTunnel

--- PantryTracker/PantryDBLibrary/DataTunnel/PantryDataTunnel.cs
+++ PantryTracker/PantryDBLibrary/DataTunnel/PantryDataTunnel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NutritionixLib.Models;
+using PantryDBLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,29 @@ namespace PantryDBLibrary.DataTunnel
 {
     public class PantryDataTunnel
     {
+        public void AddFood(Foods.Food food)
+        {
+            Food newFood = new Food(
+                food.upc,
+                food.food_name,
+                food.brand_name,
+                food.serving_qty,
+                food.serving_unit,
+                food.serving_weight_grams,
+                food.nf_calories,
+                food.nf_total_fat,
+                food.nf_saturated_fat,
+                food.nf_cholesterol,
+                food.nf_sodium,
+                food.nf_total_carbohydrate,
+                food.nf_dietary_fiber,
+                food.nf_sugars,
+                food.nf_protein,
+                food.nf_potassium,
+                food.tags.ToString(),
+                food.photo.thumb);
 
+
+        }
     }
 }
--- PantryTracker/PantryDBLibrary/Models/Food.cs
+++ PantryTracker/PantryDBLibrary/Models/Food.cs
@@ -24,5 +24,41 @@ namespace PantryDBLibrary.Models
         public string potassium;
         public string tags;
         public string photo;
+        private int? serving_qty1;
+        private int? serving_weight_grams1;
+        private int? nf_calories;
+        private int? nf_total_fat;
+        private int? nf_saturated_fat;
+        private int? nf_cholesterol;
+        private int? nf_sodium;
+        private int? nf_total_carbohydrate;
+        private int? nf_dietary_fiber;
+        private int? nf_sugars;
+        private int? nf_protein;
+        private int? nf_potassium;
+        private string v;
+        private string thumb;
+
+        public Food(int upc, string food_name, string brand_name, int? serving_qty1, string serving_unit, int? serving_weight_grams1, int? nf_calories, int? nf_total_fat, int? nf_saturated_fat, int? nf_cholesterol, int? nf_sodium, int? nf_total_carbohydrate, int? nf_dietary_fiber, int? nf_sugars, int? nf_protein, int? nf_potassium, string v, string thumb)
+        {
+            this.upc = upc;
+            this.food_name = food_name;
+            this.brand_name = brand_name;
+            this.serving_qty1 = serving_qty1;
+            this.serving_unit = serving_unit;
+            this.serving_weight_grams1 = serving_weight_grams1;
+            this.nf_calories = nf_calories;
+            this.nf_total_fat = nf_total_fat;
+            this.nf_saturated_fat = nf_saturated_fat;
+            this.nf_cholesterol = nf_cholesterol;
+            this.nf_sodium = nf_sodium;
+            this.nf_total_carbohydrate = nf_total_carbohydrate;
+            this.nf_dietary_fiber = nf_dietary_fiber;
+            this.nf_sugars = nf_sugars;
+            this.nf_protein = nf_protein;
+            this.nf_potassium = nf_potassium;
+            this.v = v;
+            this.thumb = thumb;
+        }
     }
 }
